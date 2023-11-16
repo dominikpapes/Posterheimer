e06d283bc251ede7fc5caa1a4dc22fb58f6d1ff3
dominikpapes <dp54371@fer.hr>
2023-11-16 18:56:06 +0100

    updated package.json

--- IzvorniKod/posterheimer-frontend/app.js
+++ IzvorniKod/posterheimer-frontend/app.js
@@ -1,31 +1,32 @@
-const express = require("express");
-const { createProxyMiddleware } = require("http-proxy-middleware");
-require("dotenv").config();
-const path = require("path")
+
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import dotenv from "dotenv";
+import path from "path";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 
 // Configuration
-const { PORT } = process.env;
-const { HOST } = process.env;
-const { API_BASE_URL } = process.env;
+const { PORT, HOST, API_BASE_URL } = process.env;
 
 // Proxy
 app.use(
-    "/api",
-    createProxyMiddleware({
-        target: API_BASE_URL,
-        changeOrigin: true,
-    })
+  "/api",
+  createProxyMiddleware({
+    target: API_BASE_URL,
+    changeOrigin: true,
+  })
 );
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(import.meta.url, 'build')));
 
 app.listen(PORT, HOST, () => {
-    console.log(`Starting Proxy at ${HOST}:${PORT}`);
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
 });
 
 app.get("*", async (req, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'))
-    }
-);
+  res.sendFile(path.join(import.meta.url, 'build', 'index.html'));
+});
