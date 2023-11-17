77a0b71787f846c40a8bcfe1ab6dcc8c0faaa7e7
dominikpapes <dp54371@fer.hr>
2023-11-17 14:35:57 +0100

    config za deploy

--- IzvorniKod/posterheimer-frontend/src/setupProxy.js
+++ IzvorniKod/posterheimer-frontend/src/setupProxy.js
@@ -1,10 +1,12 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
+const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:8080";
+
 module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8080/",
+      target: API_BASE_URL,
       changeOrigin: true,
     })
   );
