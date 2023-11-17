5e1201382ff058359d0c0286f05195597ec29076
dominikpapes <dp54371@fer.hr>
2023-11-17 12:44:04 +0100

    izmjena proxya frontend

--- IzvorniKod/posterheimer-frontend/vite.config.ts
+++ IzvorniKod/posterheimer-frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { DotenvConfigOptions, configDotenv } from 'dotenv'
 import react from '@vitejs/plugin-react'
 
 const API_BASE_URL_LOCAL = "http://localhost:8080"
+const API_BASE_URL_DEPLOY = "https://posterheimer-service.onrender.com/"
 const API_BASE_URL = process.env.API_BASE_URL
 
 // https://vitejs.dev/config/
@@ -11,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: API_BASE_URL_LOCAL,
+        target: API_BASE_URL,
         changeOrigin: true
       }
     }
