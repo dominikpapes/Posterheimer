39dfc4f02b84869cfd31e52d80563938ce65eafd
Mariocci <mp53543@fer.hr>
2024-01-06 17:18:22 +0100

    Create EmailService.java

---
+++ IzvorniKod/posterheimer-backend/src/main/java/com/rest/EmailAutomation/EmailService.java
@@ -0,0 +1,27 @@
+package com.rest;
+
+import com.domain.Konferencija;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    private JavaMailSender emailSender;
+
+    public EmailService(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    public void sendSimpleEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+
+    public void sendReminder(Konferencija conf) {
+        String bodyObavijest=
+    }
+}
