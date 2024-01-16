c8c1e31997c30c7276c77df9feba5122984c8293
Mariocci <mp53543@fer.hr>
2024-01-16 17:24:25 +0100

    debug 2

--- IzvorniKod/posterheimer-backend/src/test/java/comTest/PosterheimerBackendApplicationTests.java
+++ IzvorniKod/posterheimer-backend/src/test/java/comTest/PosterheimerBackendApplicationTests.java
@@ -1,9 +1,10 @@
 package comTest;
 
+import com.PosterheimerBackendApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PosterheimerBackendApplication.class)
 class PosterheimerBackendApplicationTests {
 
 	@Test
