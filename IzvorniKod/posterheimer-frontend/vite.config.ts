6abaaaa2b0ab83dc7eb65824f6141bb5d5a03792
dominikpapes <dp54371@fer.hr>
2024-01-17 13:17:44 +0100

    promjena localhost -> 127.0.0.1

--- IzvorniKod/posterheimer-frontend/vite.config.ts
+++ IzvorniKod/posterheimer-frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { DotenvConfigOptions, configDotenv } from 'dotenv'
 import react from '@vitejs/plugin-react'
 
-const API_BASE_URL_LOCAL = "http://localhost:8080/api/"
+const API_BASE_URL_LOCAL = "http://127.0.0.1:8080/api/"
 const API_BASE_URL_DEPLOY = "https://posterheimer-service.onrender.com/api/"
 const API_BASE_URL = process.env.API_BASE_URL
 
